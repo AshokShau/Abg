3b07d2db83b361396d62ef58dbfe8e7d857e01c9
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-06-28 11:38:30 +0530

    ntg

--- Abg/helpers/human_read.py
+++ Abg/helpers/human_read.py
@@ -9,7 +9,7 @@ def get_readable_time(seconds: int) -> str:
     count = 0
     ping_time = ""
     time_list = []
-    time_suffix_list = ["sᴇᴄ.", "ᴍɪɴ.", "ʜᴏᴜʀ", "ᴅᴀʏ", "ᴡᴇᴇᴋ", "ᴍᴏɴᴛʜ", "ʏᴇᴀʀ"]
+    time_suffix_list = ["sᴇᴄ", "ᴍɪɴ", "ʜᴏᴜʀ", "ᴅᴀʏ", "ᴡᴇᴇᴋ", "ᴍᴏɴᴛʜ", "ʏᴇᴀʀ"]
     while count < 4:
         count += 1
         remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
