7cffa06395ef82962a7fcc2917678dfc4dda816d
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-06-29 16:47:10 +0530

    kk

--- Abg/helpers/msg_types.py
+++ Abg/helpers/msg_types.py
@@ -1,5 +1,6 @@
 from enum import IntEnum, unique
 
+from pyrogram import Client
 from pyrogram.types import Message
 
 
@@ -16,6 +17,22 @@ class Types(IntEnum):
     ANIMATION = 9
     ANIMATED_STICKER = 10
     CONTACT = 11
+
+async def send_cmd(c: Client, msgtype: int):
+    GET_FORMAT = {
+        Types.TEXT.value: c.send_message,
+        Types.DOCUMENT.value: c.send_document,
+        Types.PHOTO.value: c.send_photo,
+        Types.VIDEO.value: c.send_video,
+        Types.STICKER.value: c.send_sticker,
+        Types.AUDIO.value: c.send_audio,
+        Types.VOICE.value: c.send_voice,
+        Types.VIDEO_NOTE.value: c.send_video_note,
+        Types.ANIMATION.value: c.send_animation,
+        Types.ANIMATED_STICKER.value: c.send_sticker,
+        Types.CONTACT: c.send_contact,
+    }
+    return GET_FORMAT[msgtype]
 
 
 async def get_note_type(m: Message):
