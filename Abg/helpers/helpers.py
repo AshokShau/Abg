f0961e540b148ee1968915ab4c4a6cbf97d22b6b
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-06-30 17:09:15 +0530

    <

--- Abg/helpers/helpers.py
+++ Abg/helpers/helpers.py
@@ -20,6 +20,40 @@ def ikb(rows=[]):
     return InlineKeyboardMarkup(inline_keyboard=lines)
     # return {'inline_keyboard': lines}
 
+def ikb2(rows=None, back=False, todo="start_back"):
+    # rows = pass the rows
+    # back - if want to make back button
+    # todo - callback data of back button
+
+    if rows is None:
+        rows = []
+    lines = []
+    try:
+        for row in rows:
+            line = []
+            for button in row:
+                btn_text = button.split(".")[1].upper()
+                button = btn(btn_text, button)  # InlineKeyboardButton
+                line.append(button)
+            lines.append(line)
+    except AttributeError:
+        for row in rows:
+            line = []
+            for button in row:
+                button = btn(*button)  # Will make the kb which don't have "." in them
+                line.append(button)
+            lines.append(line)
+    except TypeError:
+        # make a code to handel that error
+        line = []
+        for button in rows:
+            button = btn(*button)  # InlineKeyboardButton
+            line.append(button)
+        lines.append(line)
+    if back:
+        back_btn = [(btn("« ʙᴀᴄᴋ", todo))]
+        lines.append(back_btn)
+    return InlineKeyboardMarkup(inline_keyboard=lines)
 
 def btn(text, value, type="callback_data"):
     return InlineKeyboardButton(text, **{type: value})
@@ -84,37 +118,3 @@ def array_chunk(input, size):
     return [input[i : i + size] for i in range(0, len(input), size)]
 
 
-def ikb2(rows=None, back=False, todo="start_back"):
-    # rows = pass the rows
-    # back - if want to make back button
-    # todo - callback data of back button
-
-    if rows is None:
-        rows = []
-    lines = []
-    try:
-        for row in rows:
-            line = []
-            for button in row:
-                btn_text = button.split(".")[1].upper()
-                button = btn(btn_text, button)  # InlineKeyboardButton
-                line.append(button)
-            lines.append(line)
-    except AttributeError:
-        for row in rows:
-            line = []
-            for button in row:
-                button = btn(*button)  # Will make the kb which don't have "." in them
-                line.append(button)
-            lines.append(line)
-    except TypeError:
-        # make a code to handel that error
-        line = []
-        for button in rows:
-            button = btn(*button)  # InlineKeyboardButton
-            line.append(button)
-        lines.append(line)
-    if back:
-        back_btn = [(btn("« ʙᴀᴄᴋ", todo))]
-        lines.append(back_btn)
-    return InlineKeyboardMarkup(inline_keyboard=lines)
