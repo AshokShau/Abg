1d4dd2ab85f6694d1857de399f608cebb27fe849
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-07-06 12:16:07 +0530

    <3

--- Abg/__init__.py
+++ Abg/__init__.py
@@ -1,5 +1,15 @@
+import logging as log
 
-__copyright__ = "Copyright (C) 2023-present Abishnoi69 <https://github.com/Abishnoi69>"
+from datetime import datetime
+
+__version__ = "2.2"
+__copyright__ = f"Copyright 2023 - {datetime.now().year} Abishnoi69 <github.com/Abishnoi69>"
+
+LOGGER = log.getLogger("Abg")
 
 from . import chat_status, inline
+
+LOGGER.info(f"Version: {__version__}\nCopyright: {__copyright__}")
+
+
 
