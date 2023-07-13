235785248a944a07cc5d09f5b61b8b14659ec231
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-07-13 10:45:47 +0530

    bumb version and remove chat status

--- Abg/__init__.py
+++ Abg/__init__.py
@@ -1,14 +1,14 @@
 import logging as log
 from datetime import datetime
 
-__version__ = "2.3.1"
+__version__ = "2.3.2"
 __copyright__ = (
     f"Copyright 2023 - {datetime.now().year} Abishnoi69 <github.com/Abishnoi69>"
 )
 
 LOGGER = log.getLogger("Abg")
 
-from . import chat_status, inline, patch
+from . import inline, patch
 
 LOGGER.info(f"Version: {__version__}\nCopyright: {__copyright__}")
 
