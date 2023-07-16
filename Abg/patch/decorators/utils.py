b2a1987bdaae47503675eabf5bf56b3ef6e7efc2
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-07-16 22:37:51 +0530

    Minor change

--- Abg/patch/decorators/utils.py
+++ Abg/patch/decorators/utils.py
@@ -1,5 +1,4 @@
 import contextlib
-import logging
 import os
 import traceback
 import typing
@@ -7,8 +6,12 @@ from datetime import datetime
 
 import pyrogram
 
-OWNER_ID = int(os.environ.get("OWNER_ID"))
-LOGGER_ID = int(os.environ.get("LOGGER_ID"))
+try:
+    OWNER_ID = int(os.environ.get("OWNER_ID"))
+except ValueError:
+    raise Exception("Your OWNER_ID env variable is not a valid integer.")
+
+LOGGER_ID = os.environ.get("LOGGER_ID")
 
 log_chat = OWNER_ID if LOGGER_ID is None else LOGGER_ID
 
@@ -16,9 +19,6 @@ log_chat = OWNER_ID if LOGGER_ID is None else LOGGER_ID
 async def handle_error(
     error, m: typing.Union[pyrogram.types.Message, pyrogram.types.CallbackQuery]
 ):
-    logging = logging.getLogger(__name__)
-    logging.exception(traceback.format_exc())
-
     day = datetime.now()
     tgl_now = datetime.now()
     cap_day = f"{day.strftime('%A')}, {tgl_now.strftime('%d %B %Y %H:%M:%S')}"
@@ -42,7 +42,7 @@ async def handle_error(
         with contextlib.suppress(Exception):
             await m.message.delete()
             await m.message.reply_text(
-                "ᴀɴ ɪɴᴛᴇʀɴᴀʟ ᴇʀʀᴏʀ ᴏᴄᴄᴜʀʀᴇᴅ ᴡʜɪʟᴇ ᴘʀᴏᴄᴇssɪɴɢ ʏᴏᴜʀ ᴄᴏᴍᴍᴀɴᴅ.\nSorry ғᴏʀ ɪɴᴄᴏɴᴠᴇɴɪᴇɴᴄᴇ"
+                "ᴀɴ ɪɴᴛᴇʀɴᴀʟ ᴇʀʀᴏʀ ᴏᴄᴄᴜʀʀᴇᴅ ᴡʜɪʟᴇ ᴘʀᴏᴄᴇssɪɴɢ ʏᴏᴜʀ ᴄᴏᴍᴍᴀɴᴅ.\nsᴏʀʀʏ ғᴏʀ ɪɴᴄᴏɴᴠᴇɴɪᴇɴᴄᴇ"
             )
             await m.message._client.send_document(
                 log_chat,
