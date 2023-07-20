66b8918a523716516fabbd05b5735a71c1fdb632
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-07-20 13:14:34 +0530

    just a test

---
+++ Abg/config.py
@@ -0,0 +1,19 @@
+import os
+
+from prettyconf import Configuration
+from prettyconf.loaders import EnvFile, Environment
+
+config = Configuration(loaders=[Environment(), EnvFile(filename=f"{os.getcwd()}/.env")])
+
+
+class Config:
+
+    OWNER_ID = int(config("OWNER_ID"))
+    LOGGER_ID = config("LOGGER_ID")
+    DEV_USERS = [
+        int(i)
+        for i in config(
+            "DEV_USERS",
+            default="",
+        ).split(" ")
+    ]
