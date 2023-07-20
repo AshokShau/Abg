007dcb97e130a12c5e6b685a6d52b7972d4ec29e
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-07-20 13:17:01 +0530

    import from config because this think not working for vps

--- Abg/patch/decorators/adminsOnly.py
+++ Abg/patch/decorators/adminsOnly.py
@@ -11,20 +11,11 @@ from pyrogram.enums import ChatMemberStatus, ChatType
 from pyrogram.methods import Decorators
 from pyrogram.types import CallbackQuery, ChatPrivileges, Message
 
+from Abg.config import OWNER_ID, DEV_USERS
+
 LOGGER = getLogger(__name__)
 
 ANON = TTLCache(maxsize=250, ttl=30)
-
-# ENV
-try:
-    OWNER_ID = int(os.environ.get("OWNER_ID"))
-except ValueError:
-    raise Exception("Your OWNER_ID env variable is not a valid integer.")
-
-try:
-    DEV_USERS = {int(x) for x in os.environ.get("DEV_USER", "").split()}
-except ValueError:
-    raise Exception("Your DEV_USER list does not contain valid integers.")
 
 DEV_USER = list(DEV_USERS)
 DEVS = list(set([int(OWNER_ID)] + DEV_USER))
