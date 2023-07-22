51def1e107e33dc553ccc67ccc0e222a064f8486
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-07-22 11:13:26 +0530

    fixes and minor update
    • fix bug.
    • fixes alert.
    • better error handling.
    • pass dev users.
    • added allow_pm and allow_channel.
    • remove pass_anon and no_reply.

--- Abg/patch/decorators/adminsOnly.py
+++ Abg/patch/decorators/adminsOnly.py
@@ -1,5 +1,4 @@
-import typing
-from functools import wraps
+from functools import partial, wraps
 from logging import getLogger
 from typing import Union
 
@@ -7,10 +6,13 @@ import pyrogram
 from cachetools import TTLCache
 from pyrogram import Client
 from pyrogram.enums import ChatMemberStatus, ChatType
+from pyrogram.errors import UserNotParticipant
 from pyrogram.methods import Decorators
-from pyrogram.types import CallbackQuery, ChatPrivileges, Message
+from pyrogram.types import CallbackQuery, Message
 
 from Abg.config import Config
+
+from .utils import handle_error
 
 LOGGER = getLogger(__name__)
 
@@ -69,8 +71,8 @@ def adminsOnly(
     is_both: bool = False,
     only_owner: bool = False,
     only_dev: bool = False,
-    no_reply: object = False,
-    pass_anon: typing.Union[bool, bool] = False,
+    allow_pm: bool = False,
+    allow_channel: bool = False,
 ):
     """Check for permission level to perform some operations
 
@@ -81,8 +83,6 @@ def adminsOnly(
         is_both (bool, optional): If both user and bot can perform the action. Defaults to False.
         only_owner (bool, optional): If only owner can perform the action. Defaults to False. (It's Chat Owner)
         only_dev (bool, optional): if only dev users can perform the operation. Defaults to False.
-        no_reply (boot, optional): If should not reply. Defaults to False. (when isinstance is `callback` it's give alert and if isinstance is `command` it's give reply)
-        pass_anon (boot, optional): If the user is an Anonymous Admin, then it bypasses his right check.
     """
 
     def decorator(func):
@@ -91,33 +91,33 @@ def adminsOnly(
             abg: Client, message: Union[CallbackQuery, Message], *args, **kwargs
         ):
             if isinstance(message, CallbackQuery):
+                sender = partial(message.answer, show_alert=True)
                 msg = message.message
-            elif isinstance(message, Message):
-                msg = message
+                chat = message.message.chat
+                user = message.message.from_user
             else:
-                raise NotImplementedError(
-                    f"require_admin can't process updates with the type '{message.__name__}' yet."
-                )
-            chat = msg.chat
-            user = msg.from_user
+                sender = message.reply_text
+                msg = message
+                chat = message.chat
+                user = message.from_user
 
             if msg.chat.type == ChatType.PRIVATE and not (only_dev or only_owner):
-                return await func(abg, message, *args, *kwargs)
+                if allow_pm:
+                    return await func(abg, message, *args, *kwargs)
+                return await sender("ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴄᴀɴ'ᴛ ʙᴇ ᴜsᴇᴅ ɪɴ ᴀ ᴘʀɪᴠᴀᴛᴇ ᴄʜᴀᴛ.")
 
-            if msg.chat.type == ChatType.CHANNEL:
-                return await func(abg, message, *args, *kwargs)
+            if msg.chat.type == ChatType.CHANNEL and not (only_dev or only_owner):
+                if allow_channel:
+                    return await func(abg, message, *args, *kwargs)
+                return await sender("ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴄᴀɴ'ᴛ ʙᴇ ᴜsᴇᴅ ɪɴ ᴀ ᴄʜᴀɴɴᴇʟ.")
 
-            if not message.from_user and not pass_anon:
-                ANON[int(f"{msg.chat.id}{msg.id}")] = (
-                    message,
-                    func,
-                    permissions,
-                )
+            if not msg.from_user:
+                ANON[int(f"{msg.chat.id}{msg.id}")] = (msg, func, permissions)
                 keyboard = pyrogram.types.InlineKeyboardMarkup(
                     [
                         [
                             pyrogram.types.InlineKeyboardButton(
-                                text="ᴘʀᴏᴠᴇ ɪᴅᴇɴᴛɪᴛʏ ",
+                                text="✅ ᴘʀᴏᴠᴇ ɪᴅᴇɴᴛɪᴛʏ",
                                 callback_data=f"anon.{msg.id}",
                             ),
                         ]
@@ -128,40 +128,32 @@ def adminsOnly(
                     reply_markup=keyboard,
                 )
 
-            bot = (
-                await abg.get_chat_member(chat.id, abg.me.id)
-                if is_bot or is_both
-                else None
-            )
-            user = (
-                await msg.chat.get_member(msg.from_user.id)
-                if is_user or is_both
-                else None
-            )
+            try:
+                bot = await chat.get_member(abg.me.id)
+                user = await chat.get_member(message.from_user.id)
+            except UserNotParticipant:
+                return await sender(
+                    f"ᴜsᴇʀ: {message.from_user.id} ɴᴏᴛ ᴍᴀᴍʙᴇʀ ᴏғ ᴛʜɪs ᴄʜᴀᴛ."
+                )
+            except BaseException as e:
+                return await handle_error(e, msg)
+
             if only_owner:
-                user_ = await msg.chat.get_member(msg.from_user.id)
-                if user_.status == ChatMemberStatus.OWNER:
+                if user.status == ChatMemberStatus.OWNER:
                     return await func(abg, message, *args, **kwargs)
-                elif no_reply:
-                    return await msg.answer(
-                        "ᴏɴʟʏ ᴄʜᴀᴛ ᴏᴡɴᴇʀ ᴄᴀɴ ᴘᴇʀғᴏʀᴍ ᴛʜɪs ᴀᴄᴛɪᴏɴ.", show_alert=True
-                    )
                 else:
-                    return await message.reply_text(
-                        "ᴏɴʟʏ ᴄʜᴀᴛ ᴏᴡɴᴇʀ ᴄᴀɴ ᴘᴇʀғᴏʀᴍ ᴛʜɪs ᴀᴄᴛɪᴏɴ."
-                    )
+                    return await sender("ᴏɴʟʏ ᴄʜᴀᴛ ᴏᴡɴᴇʀ ᴄᴀɴ ᴘᴇʀғᴏʀᴍ ᴛʜɪs ᴀᴄᴛɪᴏɴ.")
 
             if only_dev:
                 if msg.from_user.id in DEVS:
                     return await func(abg, message, *args, **kwargs)
-                elif no_reply:
-                    return await msg.answer(
-                        "ᴏɴʟʏ ᴏɴʟʏ ʙᴏᴛ ᴅᴇᴠ ᴄᴀɴ ᴘᴇʀғᴏʀᴍ ᴛʜɪs ᴀᴄᴛɪᴏɴ.", show_alert=True
-                    )
                 else:
-                    return await message.reply_text(
+                    return await sender(
                         "ᴏɴʟʏ ʙᴏᴛ ᴅᴇᴠ ᴄᴀɴ ᴘᴇʀғᴏʀᴍ ᴛʜɪs ᴀᴄᴛɪᴏɴ.",
                     )
+
+            if msg.from_user.id in DEVS:
+                return await func(abg, message, *args, **kwargs)
 
             if permissions:
                 if permissions == "can_promote_members":
@@ -176,72 +168,66 @@ def adminsOnly(
                     no_permission = "ʙᴀɴ/ᴜɴʙᴀɴ ᴜsᴇʀs"
                 elif permissions == "can_delete_messages":
                     no_permission = "ᴅᴇʟᴇᴛᴇ ᴍᴇssᴀɢᴇs"
+                elif permissions == "can_manage_chat":
+                    no_permission = "ᴍᴀɴᴀɢᴇ ᴄʜᴀᴛ"
+                elif permissions == "can_manage_video_chats":
+                    no_permission = "ᴍᴀɴᴀɢᴇ ᴠɪᴅᴇᴏ ᴄʜᴀᴛ"
+                elif permissions == "can_post_messages":
+                    no_permission = "ᴘᴏsᴛ ᴍᴇssᴀɢᴇ"
+                elif permissions == "can_edit_messages":
+                    no_permission = "ᴇᴅɪᴛ ᴍᴇssᴀɢᴇ"
                 elif permissions == "can_manage_topics":
                     no_permission = "ᴍᴀɴᴀɢᴇ ᴛᴏᴘɪᴄs"
                 elif permissions == "is_anonymous":
                     no_permission = "ʀᴇᴍᴀɪɴ ᴀɴᴏɴʏᴍᴏᴜs"
                 if is_bot:
-                    if (
-                        getattr(bot.privileges, permissions)
-                        if isinstance(bot.privileges, ChatPrivileges)
-                        else False
-                    ):
+                    if bot.status != ChatMemberStatus.ADMINISTRATOR:
+                        return await sender("ɪ ᴍᴜsᴛ ʙᴇ ᴀᴅᴍɪɴ ᴛᴏ ᴇxᴇᴄᴜᴛᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ.")
+
+                    if getattr(bot.privileges, permissions) is True:
                         return await func(abg, message, *args, **kwargs)
-                    elif no_reply:
-                        return await msg.answer(
-                            f"ɪ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴘᴇʀᴍɪssɪᴏɴ ᴛᴏ {no_permission}",
-                            show_alert=True,
-                        )
                     else:
-                        return await message.reply_text(
+                        return await sender(
                             f"ɪ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴘᴇʀᴍɪssɪᴏɴ ᴛᴏ {no_permission}."
                         )
                 if is_user:
-                    if (
-                        getattr(user.privileges, permissions)
-                        if isinstance(user.privileges, ChatPrivileges)
-                        else False or msg.from_user.id in DEVS
-                    ):
-                        return await func(abg, message, *args, **kwargs)
-                    elif no_reply:
-                        return await msg.answer(
-                            f"ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴘᴇʀᴍɪssɪᴏɴ ᴛᴏ {no_permission}",
-                            show_alert=True,
+                    if user.status not in [
+                        ChatMemberStatus.ADMINISTRATOR,
+                        ChatMemberStatus.OWNER,
+                    ]:
+                        return await sender(
+                            "ʏᴏᴜ ᴍᴜsᴛ ʙᴇ ᴀɴ ᴀᴅᴍɪɴɪsᴛʀᴀᴛᴏʀ ᴛᴏ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ."
                         )
+
+                    if getattr(user.privileges, permissions) is True:
+                        return await func(abg, message, *args, **kwargs)
                     else:
-                        return await message.reply_text(
+                        return await sender(
                             f"ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴘᴇʀᴍɪssɪᴏɴ ᴛᴏ {no_permission}."
                         )
                 if is_both:
-                    if (
-                        getattr(bot.privileges, permissions)
-                        if isinstance(bot.privileges, ChatPrivileges)
-                        else False
-                    ):
+                    if bot.status != ChatMemberStatus.ADMINISTRATOR:
+                        return await sender("ɪ ᴍᴜsᴛ ʙᴇ ᴀᴅᴍɪɴ ᴛᴏ ᴇxᴇᴄᴜᴛᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ.")
+
+                    if getattr(bot.privileges, permissions) is True:
                         pass
-                    elif no_reply:
-                        return await msg.answer(
-                            f"ɪ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴘᴇʀᴍɪssɪᴏɴ ᴛᴏ {no_permission}",
-                            show_alert=True,
-                        )
                     else:
-                        return await message.reply_text(
+                        return await sender(
                             f"ɪ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴘᴇʀᴍɪssɪᴏɴ ᴛᴏ {no_permission}."
                         )
 
-                    if (
-                        getattr(user.privileges, permissions)
-                        if isinstance(user.privileges, ChatPrivileges)
-                        else False or msg.from_user.id in DEVS
-                    ):
-                        pass
-                    elif no_reply:
-                        return await msg.answer(
-                            f"ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴘᴇʀᴍɪssɪᴏɴ ᴛᴏ {no_permission}",
-                            show_alert=True,
+                    if user.status not in [
+                        ChatMemberStatus.ADMINISTRATOR,
+                        ChatMemberStatus.OWNER,
+                    ]:
+                        return await sender(
+                            "ʏᴏᴜ ᴍᴜsᴛ ʙᴇ ᴀɴ ᴀᴅᴍɪɴɪsᴛʀᴀᴛᴏʀ ᴛᴏ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ."
                         )
+
+                    if getattr(user.privileges, permissions) is True:
+                        pass
                     else:
-                        return await message.reply_text(
+                        return await sender(
                             f"ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴘᴇʀᴍɪssɪᴏɴ ᴛᴏ {no_permission}."
                         )
                     return await func(abg, message, *args, **kwargs)
@@ -250,7 +236,9 @@ def adminsOnly(
                         if bot.status == ChatMemberStatus.ADMINISTRATOR:
                             return await func(abg, message, *args, **kwargs)
                         else:
-                            return await message.reply_text("ɪ'ᴍ ɴᴏᴛ ᴀᴅᴍɪɴ ʜᴇʀᴇ.")
+                            return await sender(
+                                "ɪ ᴍᴜsᴛ ʙᴇ ᴀᴅᴍɪɴ ᴛᴏ ᴇxᴇᴄᴜᴛᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ."
+                            )
                     elif is_user:
                         if user.status in [
                             ChatMemberStatus.ADMINISTRATOR,
@@ -260,12 +248,16 @@ def adminsOnly(
                         elif msg.from_user.id in DEVS:
                             return await func(abg, message, *args, **kwargs)
                         else:
-                            return await message.reply_text("ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴀᴅᴍɪɴ ʜᴇʀᴇ.")
+                            return await sender(
+                                "ɪ ᴍᴜsᴛ ʙᴇ ᴀᴅᴍɪɴ ᴛᴏ ᴇxᴇᴄᴜᴛᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ."
+                            )
                     elif is_both:
                         if user.status == ChatMemberStatus.ADMINISTRATOR:
                             pass
                         else:
-                            return await message.reply_text("ɪ'ᴍ ɴᴏᴛ ᴀᴅᴍɪɴ ʜᴇʀᴇ.")
+                            return await sender(
+                                "ɪ ᴍᴜsᴛ ʙᴇ ᴀᴅᴍɪɴ ᴛᴏ ᴇxᴇᴄᴜᴛᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ."
+                            )
 
                         if user.status in [
                             ChatMemberStatus.ADMINISTRATOR,
@@ -275,7 +267,9 @@ def adminsOnly(
                         elif msg.from_user.id in DEVS:
                             pass
                         else:
-                            return await message.reply_text("ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴀᴅᴍɪɴ ʜᴇʀᴇ.")
+                            return await sender(
+                                "ʏᴏᴜ ᴍᴜsᴛ ʙᴇ ᴀɴ ᴀᴅᴍɪɴɪsᴛʀᴀᴛᴏʀ ᴛᴏ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ."
+                            )
                         return await func(abg, message, *args, **kwargs)
 
         self.add_handler(
