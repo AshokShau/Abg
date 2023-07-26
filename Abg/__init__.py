46fcf12990f261eb49f3de8b9f1b84de7518857c
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-07-26 08:53:15 +0530

    bumb version

--- Abg/__init__.py
+++ Abg/__init__.py
@@ -1,7 +1,7 @@
 import logging
 from datetime import datetime
 
-__version__ = "2.3.4"
+__version__ = "2.3.5"
 __copyright__ = (
     f"Copyright 2023 - {datetime.now().year} Abishnoi69 <github.com/Abishnoi69>"
 )
