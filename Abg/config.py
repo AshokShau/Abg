933ae1266e788822e77ca3394f4eb110ebe74ebb
Abishnoi69 <Abishnoi69@users.noreply.github.com>
2023-08-21 03:10:45 +0000

    Aᴜᴛᴏ-ғɪx

--- Abg/config.py
+++ Abg/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 
 class Config(object):
-    # Add in your env 
+    # Add in your env
     OWNER_ID = int(getenv("OWNER_ID", None))
     LOGGER_ID = getenv("LOGGER_ID", None)
     TIME_ZONE = getenv("TIME_ZONE", "Asia/Kolkata")
